a7c8a9d73486fb94e05df62768d48ac5f7b33482
ElianRamirez <eramirez38@csuchico.edu>
2025-11-19 11:51:37 -0800

    Feat: added new function for testing

--- Password.cpp
+++ Password.cpp
@@ -24,6 +24,36 @@ int Password::count_leading_characters(string phrase){
   receives a string and returns whether it has both at least one upper-case
   letter and at least one lower-case letter
 */
-bool Password::has_mixed_case(string pass){
+
+// Implementation B
+#include <cctype>
+bool Password::has_mixed_case(string str)
+{
+  bool found = false;
+  for(char c : str){
+    if( !found && c >= 'A' && c <= 'Z' ){
+      found = true;
+    }
+    else if( found && c >= 'a' && c <= 'z'){
+      return true;
+    }
+  }
   return false;
+}
+
+
+/* Receives a string and returns a count of how many case-sensitive unique characters there are. If there are duplicate instances of the same character it should only count as a single character. Even if characters look similar, as long as they have distinct ASCII values, they should qualify as unique characters. */
+unsigned int Password::unique_characters(string str)
+{
+  bool char_set[256] = {false};
+  unsigned int count = 0;
+  for (char c : str)
+  {
+    if (!char_set[(unsigned char)c])
+    {
+      char_set[(unsigned char)c] = true;
+      count++;
+    }
+  }
+  return count;
 }
--- Password.h
+++ Password.h
@@ -21,5 +21,8 @@ public:
   letter and at least one lower-case letter
   */
   bool has_mixed_case(string);
+
+unsigned int unique_characters(string str);
+
 };
 #endif
